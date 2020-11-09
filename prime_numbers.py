ec01e964a8749e3a86288bfcd1dacabc0f3da5fa
pabplagom <pabloplazag@gmail.com>
2020-11-09 16:25:34 +0100

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,5 +1,9 @@
 import time
 
+# Prime: calculate the prime numbers within the first n numbers iterating from 3 to n
+# Input: last number
+# Output: None
+
 def prime(n):
     start_time = time.time()
     print("Calculating the first %s prime numbers" %n)  
@@ -12,7 +16,11 @@ def prime(n):
         if flag ==0:
             print(prime, end=", ")
     print("\n Execution time: %s s" %(time.time()-start_time))
-    
+
+# Primeimproved2: calculate the prime numbers within the first n using and saving the prime numbers found during the process
+# Input: last number
+# Output: array of prime numbers
+
 def primeimproved2(n):
     start_time = time.time()
     print("Calculating the first %s prime numbers" %n)    
@@ -30,6 +38,11 @@ def primeimproved2(n):
             print(prime, end=", ")
     print("\n Execution time: %s s" %(time.time()-start_time))
     return primedata
+ 
+# Primeimproved2: calculate the prime numbers within the first n using and saving the prime numbers found during the process.
+# it also discards values less than half of the evaluated number
+# Input: last number
+# Output: array of prime numbers
     
 def primeimproved(n):
     start_time = time.time()
@@ -48,10 +61,14 @@ def primeimproved(n):
             print(prime, end=", ")
     print("\n Execution time: %s s" %(time.time()-start_time))
     return primedata
-    
-def factorialdescomposition(n):
+
+#factorialdecomposition: find the descomposition of a given number n
+# Input: number to be decomposed
+# Output: factors
+
+def factorialdecomposition(n):
     start_time = time.time()
-    print("Calculating the factorial descompositon of %s: " %n, end="")
+    print("Calculating the factorial decompositon of %s: " %n, end="")
     factorial = []
     i = 2
     while i <= n:
